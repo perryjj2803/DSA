65949e3ea7a7c2d99ed1115c3a44fa160a33ff1d
JJ Perry <perryjj2803@gmail.com>
2022-08-06 14:38:44 +0800

    P02.3
    numberConversion: 100%

--- P02/numberConversions.java
+++ P02/numberConversions.java
@@ -4,15 +4,30 @@ class numberConversions
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.io);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter base 10 number to convert ");
         int num = input.nextInt();
         System.out.println("What base system converting to? enter number between 2 and 12 inclusive ");
         int conversionBase = input.nextInt();
+        String result = baseConversion(num, conversionBase);
+        System.out.println("Result is " + result );
     }
 
-    public static int baseConversion(int num, int conversionBase)
+    public static String baseConversion(int num, int conversionBase)
     {
-        
+        int result = num / conversionBase;
+        int remainder = num % conversionBase;
+        String conversionResult;
+
+        if(result == 0)
+        {
+            conversionResult = Integer.toString(remainder);
+        }
+        else
+        {
+            conversionResult = baseConversion(result, conversionBase) + Integer.toString(remainder);
+        }
+        return conversionResult;
     }
+        
 }
