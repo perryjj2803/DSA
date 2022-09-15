39105a2ec02060d8bd1446053d5eb57b596d0aa0
JJPerry <perryjj2803@gmail.com>
2022-09-15 14:04:48 +0800

    15/9

---
+++ P05/DSABinarySearchTreeTestHarness.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class DSABinarySearchTreeTestHarness
+{
+    public static void main(String[] args)
+    {
+        String min, max;
+        DSABinarySearchTree BinarySearchTree = new DSABinarySearchTree();
+        BinarySearchTree.insert("A", 10);
+        BinarySearchTree.insert("B", 20);
+        BinarySearchTree.insert("C", 30);
+        BinarySearchTree.insert("D", 40);
+        BinarySearchTree.insert("E", 50);
+
+        min = BinarySearchTree.minIter();
+        max = BinarySearchTree.maxiter();
+
+        System.out.println("min key is: " + min + "and max key is: " + max );
+    }
+}
